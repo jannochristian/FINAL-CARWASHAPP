96ebece636d932271ab27d91737eb8ed6bad4974
jannochristian <jannochua.202301150@gmail.com>
2025-12-05 01:48:06 +0800

    Initial PWA commit

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { VitePWA } from 'vite-plugin-pwa';
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react(),tailwindcss(),
+  plugins: [
+    react(),
+    tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'robots.txt'],
       manifest: {
         name: 'Carwash Book App',
         short_name: 'Carwash Book',
-<<<<<<< HEAD
         description: 'Professional car care at your fingertips.',
-=======
-        description: 'Discover movie details and trailers',
->>>>>>> 51aac6625647178ec14f5a84c3f3247e1cbb6c76
         theme_color: '#000000',
         background_color: '#111111',
         display: 'standalone',
@@ -27,4 +25,4 @@ export default defineConfig({
       }
     })
   ]
-});
+})
